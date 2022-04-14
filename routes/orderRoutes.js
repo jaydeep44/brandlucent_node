ce8c1e285586be79ac7959d841f252329fb2e16f
jaydeep44 <jaydeepc721@gmail.com>
2022-04-14 18:52:40 +0530

    updated

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,9 +5,10 @@ const {
   getAllOrders,
 } = require("../controllers/orderControllers");
 const router = express.Router();
+const isverify = require("../middleware/checkAuth");
 
-router.route("/saveOrder").post(saveOrder);
-router.route("/getUserOrders/:id").get(getUserOrder);
-router.route("/getAllOrders").get(getAllOrders);
+router.route("/saveOrder").post(isverify, saveOrder);
+router.route("/getUserOrders/:id").get(isverify, getUserOrder);
+router.route("/getAllOrders").get(isverify, getAllOrders);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,16 +6,22 @@ const {
   Update_Product,
   Get_product_by_category,
 } = require("../controllers/productControllers");
+const isverify = require("../middleware/checkAuth");
+
 const { product_create } = require("../controllers/productControllers");
 
 const router = express.Router();
 
-router.route("/createProduct").post(upload.single("image"), product_create);
-router.route("/getProducts").get(Get_All_Product);
-router.route("/deleteProduct/:id").delete(Delete_product);
-router.route("/updateProduct/:id").put(upload.single("image"), Update_Product);
+router
+  .route("/createProduct")
+  .post(isverify, upload.single("image"), product_create);
+router.route("/getProducts").get(isverify, Get_All_Product);
+router.route("/deleteProduct/:id").delete(isverify, Delete_product);
+router
+  .route("/updateProduct/:id")
+  .put(isverify, upload.single("image"), Update_Product);
 router
   .route("/getProductBycategory/:id")
-  .get(upload.none(), Get_product_by_category);
+  .get(isverify, upload.none(), Get_product_by_category);
 
 module.exports = router;
